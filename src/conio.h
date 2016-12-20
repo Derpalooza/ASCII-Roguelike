b66c2735e715e52508eb4eaae52e2e6da3734d42
Fidel Musa <fxmusa@gmail.com>
2016-12-20 17:02:03 -0500

    Added disclaimer

--- src/conio.h
+++ src/conio.h
@@ -1,6 +1,11 @@
 #ifndef CONIO_H
 #define CONIO_H
 
+/*
+	DISCLAIMER: conio.h is not my code. I needed to borrow it from the Linux 'ncurses' library,
+	as the conio library is only available on Windows.
+*/
+
 /* reads from keypress, doesn't echo */
 int getch(void);
 
