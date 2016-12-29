5908235ca22c005aea1b20da534251e5ef70aa51
Fidel Musa <fxmusa@gmail.com>
2016-12-28 22:03:41 -0500

    Changed the names of functions

--- src/Level.cpp
+++ src/Level.cpp
@@ -4,27 +4,40 @@
 
 using namespace std;
 
+/*
+	TODO: 	-Refactor code so that the movement for the player and enemies use the same function
+		-Make Player and Enemy inherit from the same class
+		-Delete enemy objects after level is completed
+*/
+
 Level::Level(int stage){
 
-	// TODO: Comb level file for player position and record in player object
-		
 	string line;
 	ifstream file ("../assets/levels/leveltest2.txt");
 	
-	player = new Player();
-	 	
 	if (file.is_open()){
 	
 		while (getline(file, line)){
 			
 			levelArea.push_back(line);
-	
+			Enemy* newEnemy;
+				
 			for (int i=0; i<line.length(); i++){
 				if (line[i] == '@'){
+					
+					// Create the player object from position data
+					// TODO: make a new constructor for the player object
 					cout << "Player identified at (" << i << ", " << levelArea.size()-1 << ")" << endl;
-					player->posX = i;
-					player->posY = levelArea.size()-1;
+					player.posX = i;
+					player.posY = levelArea.size()-1;
 					break;
+				}
+				else if (line[i] == 'S'){
+				
+					// Push to the vector a pointer to the enemy
+					cout << "Enemy identified at (" << i << ", " << levelArea.size()-1 << ")" << endl;
+					newEnemy = new Enemy(i, levelArea.size()-1);
+					enemies.push_back(newEnemy);
 				}
 			}
 				
@@ -35,9 +48,9 @@ Level::Level(int stage){
 
 }
 
-void Level::handleInput(char c){
+void Level::movePlayer(char c){
 
-	int newX = player->posX, newY = player->posY;
+	int newX = player.posX, newY = player.posY;
 
 	switch(c) {
 		case 'w':
@@ -54,7 +67,7 @@ void Level::handleInput(char c){
 			break;
 	}
 
-	movePosition(player->posX, player->posY, newX, newY);
+	movePosition(player.posX, player.posY, newX, newY);
 
 }
 
@@ -76,14 +89,49 @@ void Level::movePosition(int &posX, int &posY, int &newX, int &newY){
 	}		
 }
 
-Level* Level::update(){}
+void Level::moveEnemies(){
+	
+	
+	for (int i=0; i < enemies.size(); i++){
+		int newX = enemies[i]->posX, newY = enemies[i]->posY;
+		
+		int newDir = enemies[i]->movePosition();
+			
+		switch (newDir){
+			case 1:
+				newY--;
+				cout << "Enemy " << i+1 << " moves up" << endl;
+				break;
+
+			case 2:
+				newX--;
+				cout << "Enemy " << i+1 << " moves left" << endl;
+				break;
+
+			case 3:
+				newY++;
+				cout << "Enemy " << i+1 << " moves down" << endl;
+				break;
+			
+			case 4:
+				newX++;
+				cout << "Enemy " << i+1 << " moves right" << endl;
+				break;
+		}	
+		movePosition(enemies[i]->posX, enemies[i]->posY, newX, newY);
+	
+	cout << "Enemy at (" << enemies[i]->posX << ", " << enemies[i]->posY << ")\n";
+	}
+	
+	 
+}
 
 void Level::printLevel(){
 	for (int i=0; i<levelArea.size(); i++){
 		cout << levelArea[i] << endl;
 	}
 	
-	cout << "Player position is: (" << player->posX << ", " << player->posY << ")\n"; 
+	cout << "Player position is: (" << player.posX << ", " << player.posY << ")\n"; 
 }
 
 void Level::exit(){}
--- src/Level.h
+++ src/Level.h
@@ -2,20 +2,21 @@
 #include <vector>
 #include <string>
 #include "Player.h"
+#include "Enemy.h"
 
 class Level{
 public:
 	Level(int stage);		// Load level from the level file specified by the parameter
-	void handleInput(char c);	// Make changes to the state based on the given input
- 	Level* update();		// Progress the game and return a new level if the level has been beaten
+	void movePlayer(char c);	// Make changes to the state based on the given input
+ 	void moveEnemies();		// Progress the game and return a new level if the level has been beaten
 	void printLevel();		// Print the current level state
 	void exit();			// destroys the level object (change to destructor)
 
 private:
-	
+	void moveEnemies();	
 	void movePosition(int &posX, int &posY, int &newX, int &newY);	// Moves an entity in the level from one position to another
-	
+	std::vector<Enemy*> enemies;
 	std::vector<std::string> levelArea;
 	int levelNumber;
-	Player* player;
+	Player player;
 };
